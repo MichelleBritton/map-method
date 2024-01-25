fd09892e9b647e62e3a68c355e9ee1749c6789b7
Michelle Britton <michelle@daisy-webdesign.co.uk>
2024-01-25 14:38:26 +0000

    commit

--- map.js
+++ map.js
@@ -4,16 +4,37 @@
  * command node map.js in the terminal
  */
 
+// Map method iterates over an array or object, calls the callback function on every element and returns a new array containing all teh results of that process
+// A callback function is a function that we pass as a parameter to the methd which will be called on every element of the array the method is operating on
+
 // Using a for loop
+let nums = [1, 2, 3, 4, 5];
+let results = [];
+for (let num of nums) {
+  results.push(num * 2);
+}
+console.log(results);
 
 
 // Using map()
-
+// The map method expects you to pass it a function which will be called on each element of the nums array (this is deliberately overcomplicated to demonstrate how it works)
+const multByTwo = function (num) {
+  return num * 2;
+}
+const mapResults = nums.map(multByTwo);
+console.log(mapResults);
 
 // Simplified w/ map()
+const simplified = nums.map(function (num) {return num * 2 });
+console.log(simplified);
+
 
 
 // Simplfied w/ map() + arrow function
+//Only takes one parameter, the number to be multiplied by 2, so it doesn't need brackets around num here
+const arrow = nums.map(num => num * 2);
+console.log(arrow);
+
 
 
 // With objects:
@@ -37,3 +58,7 @@ const students = [
     skill: 'CSS'
   },
 ];
+
+
+const studentsWithIds = students.map(student => [student.name, student.id, student.profession, student.skill]);
+console.log(studentsWithIds);
